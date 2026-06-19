8565304e066b16490dda2b65ef3774d055b73fa3
X-BUG <bypassdnsx@gmail.com>
2026-06-20 00:25:16 +0100

    Code admin.php

--- admin.php
+++ admin.php
@@ -69,6 +69,9 @@ foreach ($db as $d) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NETCLOUD | Radar Space</title>
     <script src="https://cdn.tailwindcss.com"></script>
