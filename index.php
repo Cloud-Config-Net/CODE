817939fbc2168d74fa3032c9d8218457b2b245d9
X-BUG <bypassdnsx@gmail.com>
2026-06-20 00:29:06 +0100

    Code index.php

--- index.php
+++ index.php
@@ -196,6 +196,9 @@ if (isset($_SESSION['temp_generated_links'])) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title>SYSTEM LOGIN | CLOUD CONFIG</title>
     <script src="https://cdn.tailwindcss.com"></script>
